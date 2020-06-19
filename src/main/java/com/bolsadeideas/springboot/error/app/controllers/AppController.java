73edc7faf315fe53c5159d304a927ac1d003d6a4
jcmartinez <jcmartinezcu@gmail.com>
2020-06-19 14:30:20 +0200

    video 109 y 110

--- src/main/java/com/bolsadeideas/springboot/error/app/controllers/AppController.java
+++ src/main/java/com/bolsadeideas/springboot/error/app/controllers/AppController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.bolsadeideas.springboot.error.app.errors.UsuarioNoEncontradoException;
 import com.bolsadeideas.springboot.error.app.models.domain.Usuario;
 import com.bolsadeideas.springboot.error.app.services.UsuarioService;
 
@@ -24,7 +25,14 @@ public class AppController {
 	
 	@GetMapping("/ver/{id}")
 	public String ver(@PathVariable Integer id, Model model) {
-		Usuario usuario = usuarioService.obtenerPorId(id);
+		/*
+		 * Usuario usuario = usuarioService.obtenerPorId(id);
+		 * 
+		 * if(usuario == null) { throw new UsuarioNoEncontradoException(id.toString());
+		 * }
+		 */
+		
+		Usuario usuario = usuarioService.obtenerPorIdOptional(id).orElseThrow(() -> new  UsuarioNoEncontradoException(id.toString()));
 		model.addAttribute("usuario", usuario);
 		model.addAttribute("titulo", "Detalle usuario: ".concat(usuario.getNombre()));
 		return "ver";
