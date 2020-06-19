0bb0ead8427b9af83a7fb038c9cdd077b2a8c656
jcmartinez <jcmartinezcu@gmail.com>
2020-06-19 13:07:42 +0200

    video 107

---
+++ src/main/java/com/bolsadeideas/springboot/error/app/services/UsuarioServiceImpl.java
@@ -0,0 +1,45 @@
+package com.bolsadeideas.springboot.error.app.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.bolsadeideas.springboot.error.app.models.domain.Usuario;
+
+@Service
+public class UsuarioServiceImpl implements UsuarioService {
+	
+	private List<Usuario> lista;
+
+	public UsuarioServiceImpl() {
+
+		this.lista = new ArrayList<>();
+		this.lista.add(new Usuario(1, "Andrés", "Guzmán"));
+		this.lista.add(new Usuario(2, "Pepa", "García"));
+		this.lista.add(new Usuario(3, "Lalo", "Mena"));
+		this.lista.add(new Usuario(4, "Luci", "Fernández"));
+		this.lista.add(new Usuario(5, "Pato", "González"));
+		this.lista.add(new Usuario(6, "Paco", "Rodríguez"));
+		this.lista.add(new Usuario(7, "Juan", "Gómez"));
+	}
+
+	@Override
+	public List<Usuario> listar() {
+		return this.lista;
+	}
+
+	@Override
+	public Usuario obtenerPorId(Integer id) {
+		Usuario resultado = null;
+		for(Usuario u: this.lista) {
+			if(u.getId().equals(id)) {
+				resultado = u;
+				break;
+			}
+		}
+		
+		return resultado;
+	}
+
+}
